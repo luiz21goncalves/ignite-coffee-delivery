312f1446224014e78940be034d348dbc9fc91429
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-08-19 19:51:09 -0300

    feat(header): show products amount with cart context

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,11 +3,16 @@ import { Link } from 'react-router-dom'
 import { useTheme } from 'styled-components'
 
 import coffeeDeliveryLogo from '../../assets/coffee-delivery-logo.svg'
+import { useCart } from '../../context/CartContext'
 
 import * as S from './styles'
 
 export function Header() {
   const { 'purple-500': purple500, 'yellow-900': yellow900 } = useTheme()
+
+  const { productsAmount } = useCart()
+
+  const formmattedProductsAmount = productsAmount < 1 ? '' : productsAmount
 
   return (
     <S.HeaderWrapper>
@@ -22,7 +27,7 @@ export function Header() {
             Porto Alegre, RS
           </S.CityLabel>
 
-          <S.CartLabel to="/checkout" data-amount={3}>
+          <S.CartLabel to="/checkout" data-amount={formmattedProductsAmount}>
             <ShoppingCart weight="fill" size={22} color={yellow900} />
           </S.CartLabel>
         </nav>
