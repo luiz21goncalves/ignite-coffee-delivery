fa5693c08a2869089ea16010c2c83e62664cad80
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-08-19 20:07:19 -0300

    refactor(counter): remove cart context and use props

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,21 +1,25 @@
 import { Minus, Plus } from 'phosphor-react'
 
-import { useCounter } from '../../context/CounterContext'
-
 import * as S from './styles'
 
-export function Counter() {
-  const { amount, decrease, increase } = useCounter()
+type CounterProps = {
+  amount: number
+  onIncrease: () => void
+  onDecrease: () => void
+}
+
+export function Counter(props: CounterProps) {
+  const { amount, onDecrease, onIncrease } = props
 
   return (
     <S.InputContainer>
-      <S.InputButtonIcon type="button" onClick={decrease}>
+      <S.InputButtonIcon type="button" onClick={onDecrease}>
         <Minus weight="bold" size={14} />
       </S.InputButtonIcon>
 
       <span>{amount}</span>
 
-      <S.InputButtonIcon type="button" onClick={increase}>
+      <S.InputButtonIcon type="button" onClick={onIncrease}>
         <Plus weight="bold" size={14} />
       </S.InputButtonIcon>
     </S.InputContainer>
