c4896e317f17582097026dd3998f1a0f0c7f4bd4
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-08-19 19:49:01 -0300

    chore(cart-context): add function to add product to cart

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,7 +1,18 @@
-import { createContext, ReactNode, useContext } from 'react'
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useReducer,
+} from 'react'
+
+import { addProductToCartAction } from '../reducers/cart/actions'
+import { cartReducer, Product } from '../reducers/cart/reducers'
 
 type CartContextData = {
   productsAmount: number
+  products: Product[]
+  addProductToCart: (data: Product) => void
 }
 
 type CartContextProviderProps = {
@@ -13,8 +24,25 @@ const CartContext = createContext({} as CartContextData)
 function CartContextProvider(props: CartContextProviderProps) {
   const { children } = props
 
+  const [cartState, dispatch] = useReducer(cartReducer, {
+    productsAmount: 0,
+    products: [],
+  })
+
+  const { products, productsAmount } = cartState
+
+  const addProductToCart = useCallback((product: Product) => {
+    dispatch(addProductToCartAction(product))
+  }, [])
+
   return (
-    <CartContext.Provider value={{ productsAmount: 0 }}>
+    <CartContext.Provider
+      value={{
+        products,
+        productsAmount,
+        addProductToCart,
+      }}
+    >
       {children}
     </CartContext.Provider>
   )
